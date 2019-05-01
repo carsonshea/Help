4e88504b3ebf26ab564ef2402359f9760ce22a5c
carsonshea <49923376+carsonshea@users.noreply.github.com>
2019-05-01 13:46:16 -0700

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,47 +1,74 @@
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public abstract class Character {
-    //private Image upMoveImg;
-    //private Image rightMoveImg;
-    //private Image downMoveImg;
-    //private Image leftMoveImg;
-    private Image upImg;
-    private Image rightImg;
-    private Image downImg;
-    private Image leftImg;
-    private Rectangle rect;
+	private Image upMoveImg;
+	private Image rightMoveImg;
+	private Image downMoveImg;
+	private Image leftMoveImg;
+	private Image upImg;
+	private Image rightImg;
+	private Image downImg;
+	private Image leftImg;
+	private Rectangle rect;
     private Robot robot;
 
-    public Character (Image up, Image right, Image down, Image left, int x, int y, int w) {
-        upImg = up;
-        rightImg = right;
-        downImg = down;
-        leftImg = left;
-        rect = new Rectangle(x,y,w,w);
+	public Character (Image up, Image right, Image down, Image left, 
+			Image moveU, Image moveR, Image moveD, Image moveL, int x, int y, int w) {
+		upImg = up;
+		rightImg = right;
+		downImg = down;
+		leftImg = left;
+		upMoveImg = moveU;
+		rightMoveImg = moveR;
+		downMoveImg = moveD;
+		leftMoveImg = moveL;
 
-    }
+		rect = new Rectangle(x,y,w,w);
+	}
 
-    public Image getUpImg() {
-        return upImg;
-    }
+	public Image getUpImg() {
+		return upImg;
+	}
 
-    public Image getLeftImg() {
-        return leftImg;
-    }
+	public Image getLeftImg() {
+		return leftImg;
+	}
 
-    public Image getDownImg() {
-        return downImg;
-    }
+	public Image getDownImg() {
+		return downImg;
+	}
 
-    public Image getRightImg() {
-        return rightImg;
-    }
+	public Image getRightImg() {
+		return rightImg;
+	}
 
-    public Rectangle getRect() {
-        return rect;
-    }
+	public Image getUpMoveImg() {
+		return upMoveImg;
+	}
+
+	public Image getLeftMoveImg() {
+		return leftMoveImg;
+	}
+
+	public Image getDownMoveImg() {
+		return downMoveImg;
+	}
+
+	public Image getRightMoveImg() {
+		return rightMoveImg;
+	}
+
+	public Rectangle getRect() {
+		return rect;
+	}
+    
     public Robot getRobot() {
         return robot;
     }
-}
 
+	//enemies: darknut,ghini, gibdo, goriya, keese, lynel, moblin, octorok, rope, stalfos, 
+}
